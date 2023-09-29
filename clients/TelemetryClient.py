1c4d81672757efe5ec791b485c2adcbc0650e10c
Parth Saxena <parths3@illinois.edu>
2023-09-28 20:21:37 -0400

    Update TelemetryClient.py

--- clients/TelemetryClient.py
+++ clients/TelemetryClient.py
@@ -1,5 +1,6 @@
 import argparse
 import asyncio
+import time
 
 class TelemetryClient:
     def __init__(self, host, port):
@@ -11,14 +12,27 @@ class TelemetryClient:
         self.reader, self.writer = await asyncio.open_connection(self.host, self.port)
         
     async def receive_telemetry(self):
+        cumulative_latency = 0.0
+        average_latency = 0.0
+        start = None
         try:
             while True:
                 data = await self.reader.read(100)
                 if not data:
                     print("Connection closed by the server")
-                    break
-                print(f'[{self.count}] {data.decode()}')
+                    break                
                 self.count += 1
+
+                if not start:
+                    start = time.time_ns()
+                else:
+                    end = time.time_ns()
+                    cumulative_latency += end - start
+                    start = end
+                    average_latency = cumulative_latency / self.count
+
+                print(f'[{self.count}] {data.decode()}   Avg. Latency: {round(average_latency / (1e9), 2)}ms')                    
+                
         except asyncio.CancelledError:
             pass
         finally:
